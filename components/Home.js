e17d21e432cb3923370e85da6162a7ef76edbac2
KarinOchayon070 <karinoch070@gmail.com>
2022-10-17 20:54:44 +0300

    Some things i learned

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,60 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { Text, View, Button, StyleSheet, TouchableOpacity } from "react-native";
 import Header from "./Header.js";
 
 const Home = () => {
-  return <Header />;
+  const [number, setNumber] = useState(5);
+
+  const karinArray = [
+    { firstName: "yovel", lastName: "ovadia" },
+    { firstName: "karin", lastName: "ochayon" },
+    { firstName: "elad", lastName: "ovadia" },
+  ];
+
+  const incrementNumber = () => {
+    if (number !== 10) {
+      setNumber(number + 1);
+    }
+  };
+
+  const decrmentNumber = () => {
+    if (number !== 0) {
+      setNumber(number - 1);
+    }
+  };
+
+  return (
+    <View>
+      <Header />
+      {/* <TouchableOpacity onPress={incrementNumber} style={styles.MamiButton}>
+        <Text>Press me to increment the number</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={decrmentNumber} style={styles.MamiButton}>
+        <Text>Press me to decrement the number</Text>
+      </TouchableOpacity>
+      <Text>{number}</Text> */}
+
+      {karinArray.map((object) => {
+        return (
+          <Text style={{ color: "red" }}>
+            {object.firstName + " " + object.lastName}
+          </Text>
+        );
+      })}
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  MamiButton: {
+    fontWeight: "bold",
+    color: "black",
+    fontSize: 50,
+    backgroundColor: "yellow",
+    alignItems: "center",
+    padding: 50,
+    justifyContent: "center",
+  },
+});
 
 export default Home;
